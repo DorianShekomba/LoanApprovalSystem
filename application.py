bbb98cfc02a5f0230cd4cf2d7a9390cbef19553a
Dorian Shekomba <deshekomba@gmail.com>
2024-08-13 23:26:29 -0500

    This is files for the loan approval system

--- application.py
+++ application.py
@@ -199,5 +199,5 @@ def detail(number):
     return render_template('detail.html', row=row)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
